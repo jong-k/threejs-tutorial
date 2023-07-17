4346d4f0fa328d217840f833696557ba7d8ffaf9
jong-k <kjhan710@naver.com>
2023-07-17 15:32:54 +0900

    init: project

---
+++ src/client/webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "../../dist"),
+    },
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+});
