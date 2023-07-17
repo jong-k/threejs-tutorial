333b242c4f2945379774a9bb05fcd7b7588a389c
jong-k <kjhan710@naver.com>
2023-07-17 16:52:01 +0900

    feat: canvas 정적으로 만들어보기

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Three.js 튜토리얼
-보일러 플레이트
+# Three.js 튜토리얼 1
+## 목차
+1. Scene, Cameras, Renderer
+
+## 1. Scene, Cameras, Renderer
+필수 요소 3가지
+- Scene : Mesh 트리
+- Camera : 
+- Renderer : HTML Canvas Element 역할
+
--- src/client/index.ts
+++ src/client/index.ts
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
-
+scene.background = new THREE.Color(0x114400); // 6자리 맞춰서 사용해야 함
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -10,8 +10,13 @@ const camera = new THREE.PerspectiveCamera(
   1000,
 );
 camera.position.z = 2;
-
+// html에서 canvas를 직접 만들어 사용해보기
+// const canvas = document.getElementById("c1") as HTMLCanvasElement;
+// const renderer = new THREE.WebGLRenderer({
+//   canvas,
+// });
 const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(300, 300);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
